662c6f1f30f3118e08e773d758a2f679c02c15ae
Do Thien Hoang <theksbd@gmail.com>
2022-12-16 16:40:25 +0700

    put info of mqtt client to .env file

--- server/mqtt/MQTTClient.js
+++ server/mqtt/MQTTClient.js
@@ -12,7 +12,7 @@ class MQTTClient {
   defineField() {
     this.feeds = ["moisture", "pump"];
     this.mqtt = require("mqtt");
-    this.username = process.env.ADAFRUIT_IO_USERNAME
+    this.username = process.env.ADAFRUIT_IO_USERNAME;
     this.port = 8883;
     this.key = process.env.ADAFRUIT_IO_KEY;
     this.url = `mqtts://${this.username}:${this.key}@io.adafruit.com`;
