f868decca7d9139f885643e1582941690f4cc240
Do Thien Hoang <theksbd@gmail.com>
2023-03-12 13:18:12 +0700

    update key

--- client/src/utils/MQTTClient.js
+++ client/src/utils/MQTTClient.js
@@ -14,7 +14,7 @@ class MQTTClient {
     this.mqtt = require("mqtt/dist/mqtt");
     this.username = "nhom3cnpm";
     this.port = 8883;
-    this.key = "aio_kXHy13kbObaYXbCgDq5kc0SSF1NF";
+    this.key = "aio_fNah836lQmoOkuhmXIcnWxWEuRjN";
     this.url = `mqtts://${this.username}:${this.key}@io.adafruit.com`;
     this.client = this.mqtt.connect(this.url, this.port);
   }
